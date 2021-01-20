354a0ce5f7990060cbb655c40e8d3e89e4bca2e6
GioTeckOfficial <gio.addy77@gmail.com>
2021-01-20 18:08:23 +0100

    Fixed some bugs

--- index.js
+++ index.js
@@ -14,19 +14,23 @@ client.on('ready',()=>{
 
 client.on('message', async (message)=>{
     const quarantine=require("./quarantine.json");
-    const channel=require("./channel.json")
+    const man=require("./channel.json")
     const after=require("./after.json");
-    const { role } = require("./role.json");
+    const use = require("./role.json").role;
     if(message.content=="invite"){
         let embed=new ds.MessageEmbed()
             .setTitle("Invitami!")
             .setDescription("Per invitare il bot usa il link qui sotto:\n https://discord.com/api/oauth2/authorize?client_id=800979258474102834&permissions=8&scope=bot")
             .setFooter("VinnyHub","https://media.discordapp.net/attachments/798827498061692928/801018255363538954/WhatsApp_Image_2020-11-24_at_17.27.41.jpeg");
         message.reply(embed);
-        var response=true;
     }else{
         if(message.guild){
             const args=message.content.split(" ");
+            for (let index = 0; index < args.length; index++) {
+                if(!args[index]){
+                    args.splice(index,1);
+                }
+            }
             const error=new ds.MessageEmbed()
                 .setTitle(":x: Errore!")
                 .setDescription("Non hai i permessi o hai formulato male il comando!")
@@ -35,10 +39,11 @@ client.on('message', async (message)=>{
             const mention=await message.mentions.members.first();
                 switch(args[0]){
                 case "/quarantine":
-                    if(!args[1]) return message.reply(error);
                     if(!mention) return message.reply(error);
-                    if(message.member.id==message.guild.ownerID){
-                        if (mention.roles.cache.some(role=>{role.id!==role})) return message.reply(error);
+                    if(message.member.id!==message.guild.ownerID){
+                        if(message.member.id!==621692762449903617){
+                            if (mention.roles.cache.some(role=>{role.id!==use})) return message.reply(error);
+                        }
                     }
                     const role_=mention.roles.cache.array();
                     for (let index = 0; index < role_.length; index++) {
@@ -55,8 +60,7 @@ client.on('message', async (message)=>{
                     await member.send(dmquarantine);
 
                     const create=new Date();
-                    var expire=new Date(create.getFullYear(),create.getMonth(),create.getDate(),create.setHours(create.getHours()+1),create.getMinutes())
-                    expire=60
+                    var expire=1;
                     function levaultimo(str){
                         len = str.length;
                         str = str.substr(0,len-1);
@@ -74,20 +78,10 @@ client.on('message', async (message)=>{
                         var raw=levaultimo(args[2]);
                         expire=raw
                     }else{
-                        expire=60
+                        expire=60;
                     }
                     console.log("Created: " + create);
                     console.log("Expire: "+expire);
-                    var reason="Hai infranto le regole del server!";
-                    if(args[3]){
-                        if(args.length>3){
-                            const reason="";
-                            for (let index = 3; index < args.length; index++) {
-                                reason.concat(args[index]);
-                                
-                            }
-                        }
-                    }
                     fs.writeFile(`./members/${mention.id}.json`,`{
                         "memberId":${mention.id},
                         "username":"${mention.user.username}",
@@ -95,14 +89,20 @@ client.on('message', async (message)=>{
                         "expiresAfter":"${expire}"
                     }`,(err)=>{
                         if(err) return console.log(err);
-                        let succ=new ds.MessageEmbed()
-                            .setTitle(":ok:")
-                            .setDescription(`${mention} è stato messo in quarantena per ${(expire)} minuti`);
-                        message.reply(succ);
+                        let channel=client.channels.cache.get(man.channel);
+                        let embed=new ds.MessageEmbed()
+                            .setTitle(message.mentions.members.first())
+                            .setDescription(`è stato/a messo in quarantena per ${expire} minuti da ${message.member}`)
+                        channel.send(embed);
                     });
                     setTimeout(async ()=>{
                         await message.mentions.members.first().roles.remove(role_e).catch(err=>console.log(err));
                         await message.mentions.members.first().roles.add(message.guild.roles.cache.find(role => role.id==after.role).id).catch(err=>console.log(err));
+                        let channel=client.channels.cache.get(man.channel);
+                        let embed=new ds.MessageEmbed()
+                            .setTitle(message.mentions.members.first())
+                            .setDescription(`è uscito dalla quarantena!`);
+                        channel.send(embed);
                     },expire*60*1000);
                     break;
                 case "/use":
@@ -110,8 +110,10 @@ client.on('message', async (message)=>{
                     if(!message.mentions.roles.first()){
                         message.reply("Non hai mezionato nessuno!");
                     }else
-                    if(message.member.id!==message.guild.ownerID){
-                        message.reply("Non sei l'owner del server!");
+                    if(message.member.id==message.guild.ownerID){
+                        if(message.member.id!==621692762449903617){
+                            if (mention.roles.cache.some(role=>{role.id!==role})) return message.reply(error);
+                        }
                     }else{
                         fs.writeFile("./role.json",`{ "role":${message.mentions.roles.first().id} }`,(err)=>{
                             if(err) return console.error(err);
@@ -125,7 +127,11 @@ client.on('message', async (message)=>{
                     break;
                 case "/set":
                     if(!args[1]) return message.reply(error);
-                    if(message.member.id!==message.guild.ownerID) return message.reply(error);
+                    if(message.member.id==message.guild.ownerID){
+                        if(message.member.id!==621692762449903617){
+                            if (mention.roles.cache.some(role=>{role.id!==role})) return message.reply(error);
+                        }
+                    }
                         const ret = message.mentions.roles.firstKey();
                         console.log(ret)
                         fs.writeFile("./quarantine.json",`{"role":${ret}}`,(err)=>{
@@ -138,6 +144,11 @@ client.on('message', async (message)=>{
                     });
                     break;
                 case "/channel":
+                    if(message.member.id==message.guild.ownerID){
+                        if(message.member.id!==621692762449903617){
+                            if (mention.roles.cache.some(role=>{role.id!==role})) return message.reply(error);
+                        }
+                    }
                     fs.writeFile('./channel.json',`{"channel":${message.channel.id}}`,(err)=>{
                         if(err) return console.log(err);
                         let succ=new ds.MessageEmbed()
@@ -149,6 +160,11 @@ client.on('message', async (message)=>{
                     break;
                 case "/after":
                     if(!args[1]) return message.reply(error);
+                    if(message.member.id==message.guild.ownerID){
+                        if(message.member.id!==621692762449903617){
+                            if (mention.roles.cache.some(role=>{role.id!==role})) return message.reply(error);
+                        }
+                    }
                     if(!message.mentions.roles.first()) return message.reply(error);
                     fs.writeFile('./after.json',`{"role":${message.mentions.roles.first().id}}`,(err)=>{
                         if(err) return console.log(err);
@@ -161,10 +177,12 @@ client.on('message', async (message)=>{
                     break;
                 case "/revoke":
                     const role_q = message.guild.roles.cache.find(role => role.id==quarantine.role);
-                    if(!args[1]) return message.reply(error);
+                    //if(!args[1]) return message.reply(error);
                     if(!mention) return message.reply(error);
                     if(message.member.id==message.guild.ownerID){
-                        if (mention.roles.cache.some(role=>{role.id!==role})) return message.reply(error);
+                        if(message.member.id!==621692762449903617){
+                            if (mention.roles.cache.some(role=>{role.id!==role})) return message.reply(error);
+                        }
                     }
                     message.mentions.members.first().roles.remove(role_q).catch(err=>console.log(err));
                     message.mentions.members.first().roles.add(message.guild.roles.cache.find(role => role.id==after.role).id).catch(err=>console.log(err));
@@ -178,12 +196,21 @@ client.on('message', async (message)=>{
                     })
                     break;
                 case "/help":
-                    if(message.member.id!==message.guild.ownerID) return message.reply(error);
+                    if(message.member.id!==621692762449903617){
+                        if(message.member.id!==message.guild.ownerID) return message.reply(error);
+                    }
                     let help=new ds.MessageEmbed()
                         .setTitle("Help")
                         .setURL('https://www.notion.so/gioteck/VinnyHub-Quarantine-6d449865cfb04dcda316acb68352437a')
                         .setDescription("Clicca sul titolo per vedere tutti i comandi!");
                         message.reply(help);
+                    break;
+                case "/test":
+                    console.log(message.member.id);
+                    for (let index = 0; index < args.length; index++) {
+                        console.log(args[index])
+                        
+                    }
                     break;
             }
         }
